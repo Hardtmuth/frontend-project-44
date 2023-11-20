2c7cd7452ec97efefff36d3ee707a6e5edf292de
Andrey Abramov <abramow-a.v@ya.ru>
2023-11-20 21:55:37 +0300

    add cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!')
+import { namedGreeting } from "../src/cli.js";
+
+
+console.log('Welcome to the Brain Games!');
+console.log(namedGreeting());
