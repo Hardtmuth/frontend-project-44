cee43274d8a5d0c34f707d365578777b4cb6a392
Hardtmuth <abramow-a.v@ya.ru>
2023-11-22 16:45:20 +0300

    start brain-even game

---
+++ src/even-game.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const brainEven = () => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    const questionsCount = 3;
+    for (let i = 0; i < questionsCount; i += 1) {
+
+    }
+    const name = readlineSync.question('May I have your name? ');
+}
