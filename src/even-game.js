9de514ea8a827235b9cfc0ca92a44eb47381fbdc
Hardtmuth <abramow-a.v@ya.ru>
2023-11-22 20:53:37 +0300

    add some logic to brain-even

--- src/even-game.js
+++ src/even-game.js
@@ -1,10 +1,26 @@
 import readlineSync from 'readline-sync';
 
-const brainEven = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    const questionsCount = 3;
-    for (let i = 0; i < questionsCount; i += 1) {
+const getRandomInt = (max=99) => Math.floor(Math.random() * max);
 
+const isEven = (int) => int % 2 === 0 ? true : false;
+
+const answerToBool = (answer) => answer === 'yes' ? true : false;
+
+const brainEven = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const questionsCount = 3;
+  for (let i = 0; i < questionsCount; i += 1) {
+    const questionInt = getRandomInt();
+    const questionIntEven = isEven(questionInt);
+
+    console.log(`Question: ${questionInt}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (questionIntEven === answerToBool(answer)) {
+      console.log('Correct!');
+    } else {
+      return `'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!`;
     }
-    const name = readlineSync.question('May I have your name? ');
+  }
+  return `Congratulations`;
 }
